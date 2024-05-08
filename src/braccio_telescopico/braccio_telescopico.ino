4fb5600184054c99880fccfa26143e21b870fdfc
Federico <91740549+federicovolpe@users.noreply.github.com>
2024-05-08 17:24:16 +0200

    funzionante ad assemblaggio terminato

--- src/braccio_telescopico/braccio_telescopico.ino
+++ src/braccio_telescopico/braccio_telescopico.ino
@@ -1,62 +1,46 @@
 //V 1.0 : il pid gestisce il tempo in cui il motore resta acceso
-
-
-# include <PID_v1.h>
-double setpoint = 10; // distanza costante desiderata
-//double input; // input dal sensore ad ultrasuoni
-//double output; // distanza da spostarsi (espressa in millisecondi di attivazione del motore)
-//double Kp = 0, Ki = 10, Kd = 0;
-//PID myPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT); // ISTANZA DEL PID
+// parametri
+const int obiettivo = 10; // obiettivo di distanza in cm 
+#define SOGLIA 1 
+#define SOUND_SPEED 0.034 
+volatile int sogliaFineCorsaAvanti, sogliaFineCorsaIndietro; // parametri di riferimento iniziali per le fotoresistenze
+volatile bool finecorsa_indietro = false, finecorsa_avanti = false;
+volatile int direzione = 1;
 
 // setup pins
 // per il motore
 const int dir1 = 13, dir2 = 12, pwmPin = 11, Signal = 0;  // apparentemente non serve
 
-// pin di interrupt fine corsa
-const int finecAvanti = A1, finecIndietro = A3;
-volatile int sogliaFineCorsaAvanti, sogliaFineCorsaIndietro;
+// pin per il semaforo
+const int semaforo_indietro = 10, semaforo_avanti = 8, semaforo_verde = 9;
 
-// per il sensore ad ultrasuoni
-const int trigPin = 5, echoPin = 4;     
-#define SOUND_SPEED 0.034
-#define CM_TO_INCH 0.393701
-long duration;
-float distanceCm;
-float distanceInch;
+// pin per le fotoresistenze di finecorsa
+const int finecAvanti = A2, finecIndietro = A1;
 
-volatile bool finecorsa_indietro = false, finecorsa_avanti = false;
-volatile int direzione = 1;
+// pin per il sensore ad ultrasuoni
+const int trigPin = A4, echoPin = A5;     
 
 void setup() {
   // pins per il semaforo
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(8, OUTPUT);
-  digitalWrite(10, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(8, HIGH);
-
-  // pin per il motore
-  pinMode(A0, INPUT);
+  pinMode(semaforo_indietro, OUTPUT); digitalWrite(semaforo_indietro, LOW);
+  pinMode(semaforo_verde, OUTPUT);    digitalWrite(semaforo_verde, LOW);
+  pinMode(semaforo_avanti, OUTPUT);   digitalWrite(semaforo_avanti, HIGH);
   
+  // pin per il motore
   pinMode(dir1, OUTPUT);
   pinMode(dir2, OUTPUT);
   pinMode(pwmPin, OUTPUT);
-  pinMode(Signal, OUTPUT);
 
-  // pin di interrupt
+  // pin di finecorsa
   pinMode(finecAvanti, INPUT);
   pinMode(finecIndietro, INPUT);
+  // inizializzazione dei valori delle resistenze per il finecorsa
   sogliaFineCorsaAvanti = analogRead(finecAvanti);
   sogliaFineCorsaIndietro = analogRead(finecIndietro);
 
   // pin per il sensore ad ultrasuoni
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-
-  // riguardo il pid
-  //myPID.SetMode(AUTOMATIC); // accensione del processo pid
-  //myPID.SetTunings(Kp, Ki, Kd); //   passaggio dei parametri per il pid
    
   Serial.begin(9600);
 
@@ -65,9 +49,8 @@ void setup() {
 }
 
 
-
 void cambioDirezione(int input) {
-  if (input > setpoint){ // se la distanza è maggiore del set point torno indietro
+  if (input > obiettivo){ // se la distanza è maggiore del set point torno indietro
       digitalWrite(dir1, HIGH);
       digitalWrite(dir2, LOW);
       direzione = 1;
@@ -94,10 +77,10 @@ int letturaUltrasuoni() {
   digitalWrite(trigPin, LOW);
   
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
+  long duration = pulseIn(echoPin, HIGH);
   
   // Calculate the distance
-  distanceCm = duration * SOUND_SPEED/2;
+  float distanceCm = duration * SOUND_SPEED/2;
   
   /* Prints the distance in the Serial Monitor*/
   Serial.print("Distance(cm):");
@@ -106,20 +89,23 @@ int letturaUltrasuoni() {
 }
 
 
-// calcola il tempo di movimento del motore e ritorna lo stesso
-int movimento(int input) {
+int movimento(int input) { // calcola il tempo di movimento del motore e ritorna lo stesso
   int tempo_movimento = 0;
   
-  if(abs(input - setpoint) > 2) {
-    if(direzione == -1){ // nel caso mi dovessi muovere indietro il valore in cm deve essere moltiplicato poichè 2ms sarebbero troppo pochi per il motore
-      tempo_movimento = map(abs(input - setpoint), 2, 10, 10, 30);
+  if(abs(input - obiettivo) > SOGLIA) { // se la distanza è off di 2cm
+    if(direzione == 0){ // nel caso mi dovessi muovere indietro il valore in cm deve essere moltiplicato poichè 2ms sarebbero troppo pochi per il motore
+      tempo_movimento = map(abs(input - obiettivo), SOGLIA, 10, 20, 40);
     }
-    tempo_movimento = map(abs(input - setpoint), 2, 10, 10, 30);
+    tempo_movimento = map(abs(input - obiettivo), SOGLIA, 200, 20, 40);
   }
+  Serial.print("------------      tempo calcolato :");
+  Serial.print(tempo_movimento);
+  Serial.println("     ------------");
   
-  analogWrite(pwmPin, 255);
+  digitalWrite(pwmPin, HIGH);
   delay(tempo_movimento);
-  analogWrite(pwmPin, 0);
+  //delay(500);
+  digitalWrite(pwmPin, LOW);
 
   return tempo_movimento;
 }
@@ -128,6 +114,47 @@ bool direzioneConcessa(){ // consento il movimento solo nella direzione libera
   if (finecorsa_indietro && direzione == 0) return false; // se posso solo andare avanti
   if (finecorsa_avanti && direzione == 1) return false;
   return true;
+}
+
+void calcolo_finecorsa(){
+  if(finecorsa_avanti && direzione == 1) {
+    Serial.println("--------non cambio");
+  }else if ( (sogliaFineCorsaAvanti - analogRead(finecAvanti)) > (sogliaFineCorsaAvanti / 5)){
+    Serial.println("--------  fine corsa avanti !!!!!! ");
+    digitalWrite(semaforo_avanti, HIGH);
+    finecorsa_avanti = true;
+  }else{
+    digitalWrite(semaforo_avanti, LOW);
+    finecorsa_avanti = false;
+  } 
+
+  if(finecorsa_indietro && direzione == 0){// non cambia se continuo a voler andare verso il finecorsa
+    Serial.println("--------non cambio");
+  }else if( (sogliaFineCorsaIndietro - analogRead(finecIndietro)) > (sogliaFineCorsaIndietro / 5)){ // altrimenti ricalcolo i finecorsa
+    Serial.println("--------  fine corsa indietro !!!!!! ");
+    digitalWrite(semaforo_indietro, HIGH);
+    finecorsa_indietro = true;
+  }else{
+    finecorsa_indietro = false;
+    digitalWrite(semaforo_indietro, LOW);
+  }
+
+  if(!finecorsa_avanti && !finecorsa_indietro){ digitalWrite(semaforo_verde, HIGH);}
+  else {                                        digitalWrite(semaforo_verde, LOW); }
+}
+
+void stampaParametri () {
+  /*Serial.print("finecorsa Indietro:");
+  Serial.println(finecorsa_indietro);
+  Serial.print("finecorsa Avanti:");
+  Serial.println(finecorsa_avanti); */
+
+  Serial.print("sensore_avanti:");
+  Serial.println(analogRead(finecAvanti));
+  Serial.print("sensore_indietro:");
+  Serial.println(analogRead(finecIndietro));
+  
+  Serial.println("---------------------------------------");
 }
 
 
@@ -139,53 +166,17 @@ void loop() {
   //***************************************** movimento effettivo del braccio ********************************************
   int delta_mov = 0;
   if(direzioneConcessa()) { // se mi posso muovere
-    delta_mov = movimento(input);  
+    delta_mov = movimento(input); 
   }
 
   //**************************** calcolo dei prarametri di fine corsa ***************************
-  if(finecorsa_avanti && direzione == 1) {
-    Serial.println("--------non cambio");
-  }else if ( (sogliaFineCorsaAvanti - analogRead(finecAvanti)) > (sogliaFineCorsaAvanti / 5)){
-    Serial.println("--------  fine corsa avanti !!!!!! ");
-    digitalWrite(8, HIGH);
-    finecorsa_avanti = true;
-  }else{
-    digitalWrite(8, LOW);
-    finecorsa_avanti = false;
-  } 
-
-  if(finecorsa_indietro && direzione == 0){// non cambia se continuo a voler andare verso il finecorsa
-    Serial.println("--------non cambio");
-  }else if( (sogliaFineCorsaIndietro - analogRead(finecIndietro)) > (sogliaFineCorsaIndietro / 5)){ // altrimenti ricalcolo i finecorsa
-    Serial.println("--------  fine corsa indietro !!!!!! ");
-    digitalWrite(10, HIGH);
-    finecorsa_indietro = true;
-  }else{
-    finecorsa_indietro = false;
-    digitalWrite(10, LOW);
-  }
-  
+  calcolo_finecorsa();
 
   //***************************************** stampa dei parametri di controllo ********************************************
-  /*Serial.print("movimento:");
-  if(direzione == 1){
-  Serial.println(delta_mov);  
-  }else{
-    Serial.println(- delta_mov);  
-  }
-  /*Serial.print("finecorsa Indietro:");
-  Serial.println(finecorsa_indietro);
-  Serial.print("finecorsa Avanti:");
-  Serial.println(finecorsa_avanti); */
+  Serial.print("movimento:");
+  if ( direzione == 1 ){   Serial.println(delta_mov);  }
+  else{                   Serial.println(- delta_mov);  }
 
-  Serial.print("sensore_avanti:");
-  Serial.println(analogRead(finecAvanti));
-  Serial.print("sensore_indietro:");
-  Serial.println(analogRead(finecIndietro));
-
-  // segnalazione semaforo finecorsa
-  if(!finecorsa_avanti && !finecorsa_indietro){digitalWrite(9, HIGH);}else {digitalWrite(9, LOW);}
-  Serial.println("---------------------------------------");
-
-  delay(200); //delay di sicurezza per nonfare bruciare il motore
+  stampaParametri();
+  //delay(30);
 }
